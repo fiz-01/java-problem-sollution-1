feaaa13c65bb354a361e6271562370d298c6f33a
Md. Mostafiz Ul Islam <86643222+fiz-01@users.noreply.github.com>
2023-12-31 17:25:03 +0600

    Sum of array

---
+++ src/solution.java
@@ -0,0 +1,16 @@
+public class solution {
+
+
+        public static void main(String[] args) {
+            int[] numbers = {22, 11, 42, 53, 74};
+            int sum=0;
+            for (int i = 0; i < numbers.length; i++) {
+             sum=sum+numbers[i];
+            }
+            System.out.println("The sum is: " + sum);
+        }
+    }
+
+
+
+
