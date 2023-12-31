55ae8c69b87927de4e2a897bed7bda97ccde748a
Md. Mostafiz Ul Islam <86643222+fiz-01@users.noreply.github.com>
2023-12-31 17:43:03 +0600

    corrected version of sum

--- src/solution.java
+++ src/solution.java
@@ -1,16 +1,24 @@
 public class solution {
 
 
-        public static void main(String[] args) {
-            int[] numbers = {22, 11, 42, 53, 74};
-            int sum=0;
-            for (int i = 0; i < numbers.length; i++) {
-             sum=sum+numbers[i];
-            }
-            System.out.println("The sum is: " + sum);
-        }
+    public static void main(String[] args) {
+        int[] numbers = {82, 24, 94, 22, 42};
+        int result = sum(numbers);
+        System.out.println("Sum of the numbers: " + result);
     }
 
+
+    public static int sum(int[] numbers) {
+
+
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+        return sum;
+    }
+
+}
 
 
 
